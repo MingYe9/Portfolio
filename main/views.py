dc49eb996e2966df746bf67b59b08759cff2546f
Ming Ye <mye2@ualberta.ca>
2023-10-26 00:09:42 -0600

    Add view.py for main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+		UserProfile,
+		Blog,
+		Portfolio,
+		Testimonial,
+		Certificate
+	)
 
-# Create your views here.
+from django.views import generic
+from . forms import ContactForm
+
+
+class IndexView(generic.TemplateView):
+    # Home page
+	template_name = "main/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		
+		testimonials = Testimonial.objects.filter(is_active=True)
+		certificates = Certificate.objects.filter(is_active=True)
+		blogs = Blog.objects.filter(is_active=True)
+		portfolio = Portfolio.objects.filter(is_active=True)
+		
+		context["testimonials"] = testimonials
+		context["certificates"] = certificates
+		context["blogs"] = blogs
+		context["portfolio"] = portfolio
+		return context
+
